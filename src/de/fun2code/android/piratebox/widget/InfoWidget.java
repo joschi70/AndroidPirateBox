67102e67401f96d615a725714a6452d3fb6b1efd
joschi70 <joschi@fun2code.de>
2014-05-24 13:21:09 +0200

    Total connection count added to Info Widget

--- src/de/fun2code/android/piratebox/widget/InfoWidget.java
+++ src/de/fun2code/android/piratebox/widget/InfoWidget.java
@@ -7,27 +7,19 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
+import de.fun2code.android.piratebox.Constants;
 import de.fun2code.android.piratebox.R;
 
 public class InfoWidget extends AppWidgetProvider {
 
 	private static String TAG = "PirateBoxInfoWidget";
 	
-	private final String BROADCAST_INTENT_SERVER = "de.fun2code.android.piratebox.broadcast.intent.SERVER";
-	private final String BROADCAST_INTENT_SHOUT = "de.fun2code.android.piratebox.broadcast.intent.SHOUT";
-	private final String BROADCAST_INTENT_UPLOAD = "de.fun2code.android.piratebox.broadcast.intent.UPLOAD";
-	private final String BROADCAST_INTENT_STATUS_REQUEST = "de.fun2code.android.piratebox.broadcast.intent.STATUS_REQUEST";
-	private final String BROADCAST_INTENT_STATUS_RESULT = "de.fun2code.android.piratebox.broadcast.intent.STATUS_RESULT";
-	
-	private final String INTENT_SERVER_EXTRA_STATE = "SERVER_STATE";
-	private final String INTENT_UPLOAD_EXTRA_NUMBER = "UPLOAD_NUMBER";
-	private final String INTENT_SHOUT_EXTRA_NUMBER = "SHOUT_NUMBER";
-	
 	private final String WIDGET_INTENT_CLICKED = "de.fun2code.android.piratebox.infowidget.intent.clicked";
 	
 	private static boolean serverStatus = false;
 	private static int uploads = 0;
 	private static int shouts = 0;
+	private static int connections = 0;
 	
 
 	@Override
@@ -39,29 +31,34 @@ public class InfoWidget extends AppWidgetProvider {
 				InfoWidget.class));
 		//Toast.makeText(context, intent.getAction(), Toast.LENGTH_SHORT).show();
 		if(intent.getAction()
-				.equals(BROADCAST_INTENT_SERVER)) {
-			serverStatus = intent.getBooleanExtra(INTENT_SERVER_EXTRA_STATE, false);
+				.equals(Constants.BROADCAST_INTENT_SERVER)) {
+			serverStatus = intent.getBooleanExtra(Constants.INTENT_SERVER_EXTRA_STATE, false);
 		}
 		else if(intent.getAction()
-				.equals(BROADCAST_INTENT_STATUS_RESULT)) {
-			serverStatus = intent.getBooleanExtra(INTENT_SERVER_EXTRA_STATE, false);
-			uploads = intent.getIntExtra(INTENT_UPLOAD_EXTRA_NUMBER, 0);
-			shouts = intent.getIntExtra(INTENT_SHOUT_EXTRA_NUMBER, 0);
+				.equals(Constants.BROADCAST_INTENT_STATUS_RESULT)) {
+			serverStatus = intent.getBooleanExtra(Constants.INTENT_SERVER_EXTRA_STATE, false);
+			uploads = intent.getIntExtra(Constants.INTENT_UPLOAD_EXTRA_NUMBER, 0);
+			shouts = intent.getIntExtra(Constants.INTENT_SHOUT_EXTRA_NUMBER, 0);
+			connections = intent.getIntExtra(Constants.INTENT_CONNECTION_EXTRA_NUMBER, 0);
 		}
 		else if(intent.getAction()
-				.equals(BROADCAST_INTENT_UPLOAD)) {
+				.equals(Constants.BROADCAST_INTENT_UPLOAD)) {
 			uploads++;
 		}
 		else if(intent.getAction()
-				.equals(BROADCAST_INTENT_SHOUT)) {
+				.equals(Constants.BROADCAST_INTENT_SHOUT)) {
 			shouts++;
+		}
+		else if(intent.getAction()
+				.equals(Constants.BROADCAST_INTENT_CONNECTION)) {
+			connections++;
 		}
 		else if(intent.getAction()
 				.equals(WIDGET_INTENT_CLICKED) || 
 				intent.getAction()
 				.equals(AppWidgetManager.ACTION_APPWIDGET_UPDATE)) {
 			// Send broadcast to request info
-			context.sendBroadcast(new Intent(BROADCAST_INTENT_STATUS_REQUEST));
+			context.sendBroadcast(new Intent(Constants.BROADCAST_INTENT_STATUS_REQUEST));
 		}
 		
 		
@@ -72,7 +69,7 @@ public class InfoWidget extends AppWidgetProvider {
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
 			int[] ints) {
-		context.sendBroadcast(new Intent(BROADCAST_INTENT_STATUS_REQUEST));
+		context.sendBroadcast(new Intent(Constants.BROADCAST_INTENT_STATUS_REQUEST));
 		showWidget(context, appWidgetManager, ints);
 	}
 
@@ -89,6 +86,7 @@ public class InfoWidget extends AppWidgetProvider {
 		//views.setTextViewText(R.id.textStatus, serverStatus ? "up" : "down");
 		views.setTextViewText(R.id.textUploads, String.valueOf(uploads));
 		views.setTextViewText(R.id.textShouts, String.valueOf(shouts));
+		views.setTextViewText(R.id.textConnections, String.valueOf(connections));
 		
 		Intent msg = new Intent(WIDGET_INTENT_CLICKED);
         PendingIntent intent = PendingIntent.getBroadcast(context, -1 /*not used*/, msg, PendingIntent.FLAG_UPDATE_CURRENT);
@@ -106,7 +104,7 @@ public class InfoWidget extends AppWidgetProvider {
 	public void onEnabled(Context context) {
 		super.onEnabled(context);
 		
-		context.sendBroadcast(new Intent(BROADCAST_INTENT_STATUS_REQUEST));
+		context.sendBroadcast(new Intent(Constants.BROADCAST_INTENT_STATUS_REQUEST));
 	}
 
 }

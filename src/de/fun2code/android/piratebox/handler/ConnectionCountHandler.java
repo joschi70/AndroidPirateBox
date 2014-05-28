fb2962c2ef7cc100ce43bccbb741eb8241ecb157
joschi70 <joschi@fun2code.de>
2014-05-28 20:02:43 +0200

    Connection counter uses LibraryBox schematic (REMOTE_ADDRESS + USER_AGENT_STRING + DATE)

--- src/de/fun2code/android/piratebox/handler/ConnectionCountHandler.java
+++ src/de/fun2code/android/piratebox/handler/ConnectionCountHandler.java
@@ -4,7 +4,13 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 import sunlabs.brazil.server.Handler;
@@ -18,9 +24,9 @@ import de.fun2code.android.piratebox.PirateBoxService;
 
 /**
  * Handler that counts user sessions and connections (MAC addresses).
- * To count user sessions the session cookie is used that is provided by the
- * {@code sunlabs.brazil.handler.CookieSessionHandler}
- * 
+ * To count user sessions the SHA1-Hash of the following combination is used:<br/>
+ * {@code REMOTE_ADDRESS + USER_AGENT_STRING + DATE}
+ * <br/>
  * To get the MAC address that corresponds to an IP number, the file
  * {@literal /proc/net/arp} is inspected.
  * 
@@ -29,16 +35,15 @@ import de.fun2code.android.piratebox.PirateBoxService;
  */
 public class ConnectionCountHandler implements Handler {
 	private String prefix;
-	private static String COOKIE_NAME = "cookie";
 	private static final String ARP_FILE = "/proc/net/arp";
 	
-	private static Set<String> cookies = new HashSet<String>();
+	private static Set<String> sha1Hashes = new HashSet<String>();
 	private static Set<String> macAddresses = new HashSet<String>();
 
 	@Override
 	public boolean init(Server server, String prefix) {
 		this.prefix = prefix;
-		cookies.clear();
+		sha1Hashes.clear();
 		macAddresses.clear();
 		
 		return true;
@@ -46,17 +51,17 @@ public class ConnectionCountHandler implements Handler {
 
 	@Override
 	public boolean respond(Request request) throws IOException {
-		String cookie = getSessionCookie(request);
-		
+		String sha1 = getSha1Hash(request);
+
 		/*
 		 * If cookie is not in list of cookies the file /proc/net/arp
 		 * has to be inspected.
 		 */
-		if(cookie != null && !cookies.contains(cookie)) {
-			cookies.add(cookie);
+		if(sha1 != null && !sha1Hashes.contains(sha1)) {
+			sha1Hashes.add(sha1);
 			// Send new session broadcast
 			Intent intentSession = new Intent(Constants.BROADCAST_INTENT_CONNECTION);
-			intentSession.putExtra(Constants.INTENT_CONNECTION_EXTRA_NUMBER, cookies.size());
+			intentSession.putExtra(Constants.INTENT_CONNECTION_EXTRA_NUMBER, sha1Hashes.size());
 			PirateBoxService.getService().sendBroadcast(intentSession);
 			
 			String mac = getMacAddress(request);
@@ -70,26 +75,52 @@ public class ConnectionCountHandler implements Handler {
 				intentConnection.putExtra(Constants.INTENT_SESSION_EXTRA_NUMBER, macAddresses.size());
 				PirateBoxService.getService().sendBroadcast(intentConnection);
 			}
+			
+			// TODO: Write data to db
 		}
 		return false;
 	}
 	
 	/**
-	 * Returns the cookie that belongs to the request.
-	 * If cookie was not found {@code null} is returned.
+	 * Returns the SHA-1 hash of the following information:<br/>
+	 * {@code REMOTE_ADDRESS + USER_AGENT_STRING + DATE}
+	 * <br/>
+	 * This is done the same was as in LibraryBoy.
+	 * <br/>
+	 * see: {@link https://github.com/LibraryBox-Dev/LibraryBox-core/blob/master/customization/www_librarybox/vc_counter.php}
 	 * 
-	 * @param request	Request to use
-	 * @return			cookie or {@code null} if no cookie was found
+	 * @param request
+	 * @return
 	 */
-	private String getSessionCookie(Request request) {
-		String cookieRes = null;
+	private String getSha1Hash(Request request) {
+		String remoteAddr = request.getSocket().getInetAddress().getHostAddress();
+		String userAgent = request.headers.get("User-Agent");
+		String date = new SimpleDateFormat("yyyy-MM-dd", Locale.US).format(new Date());
 		
-		if(request.headers.get("Cookie") != null) {
-			cookieRes = request.headers.get("Cookie").replaceAll("^.*?" + COOKIE_NAME + "=([^;]*).*$", "$1");
-		}
-		
-		return cookieRes != null && !cookieRes.equals(request.headers.get("Cookie")) ? cookieRes: null;
+		return sha1(remoteAddr + userAgent + date);
 	}
+	
+	/**
+	 * Calculates SHA-1 hash
+	 * <br/>
+	 * Code taken from: {@link http://www.androidsnippets.com/sha-1-hash-function}
+	 * 
+	 * @param s		String to use for hash calculation
+	 * @return		SHA-1 hex value
+	 */
+	public String sha1(String s) {
+		MessageDigest digest = null;
+		try {
+			digest = MessageDigest.getInstance("SHA-1");
+		} catch (NoSuchAlgorithmException e) {
+			Log.e(Constants.TAG, e.toString());
+		}
+		digest.reset();
+		byte[] data = digest.digest(s.getBytes());
+		return String.format("%0" + (data.length * 2) + "X", new BigInteger(1,
+				data));
+	}
+
 	
 	/**
 	 * Retrieves the IP number of the request and returns the matching MAC
@@ -144,7 +175,7 @@ public class ConnectionCountHandler implements Handler {
 	 * @return		number of total sessions
 	 */
 	public static int getSessionCount() {
-		return cookies.size();
+		return sha1Hashes.size();
 	}
 
 }

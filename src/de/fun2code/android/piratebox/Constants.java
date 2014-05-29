31c2926f46e8dd5ca740a65846bf937f3981ee0c
joschi70 <joschi@fun2code.de>
2014-05-29 09:55:35 +0200

    New "Reset Networking" preference which resets all networking redirects

--- src/de/fun2code/android/piratebox/Constants.java
+++ src/de/fun2code/android/piratebox/Constants.java
@@ -34,10 +34,12 @@ public class Constants {
 	public static final String PREF_DEV_INFO_MESSAGES = "infoMessages";
 	public static final String PREF_DEV_INFO_CONNECTIONS = "infoConnections";
 	public static final String PREF_DEV_RESTORE_DNSMASQ = "restoreDnsMasq";
+	public static final String PREF_DEV_RESET_NETWORKING = "resetNetworking";
 	public static final String PREF_EMULATE_DROOPY = "emulateDroopy";
 	public static final String PREF_KEEP_DEVICE_ON = "keepDeviceOn";
 	
 	public static final String AP_IP_DEFAULT = "192.168.43.1"; // Default AP IP address
+	public static final String NAT_TABLE_NAME = "nat";
 	
 	//public static final String DEV_SWITCH_FILE = Environment.getExternalStorageDirectory().getPath() + "/.piratebox_dev";
 	

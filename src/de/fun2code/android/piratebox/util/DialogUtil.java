fcc162e238a9c22c8b1795f4bb29e68cf98f4111
joschi70 <joschi@fun2code.de>
2014-06-05 18:01:29 +0200

    Added input dialog

--- src/de/fun2code/android/piratebox/util/DialogUtil.java
+++ src/de/fun2code/android/piratebox/util/DialogUtil.java
@@ -1,8 +1,10 @@
 package de.fun2code.android.piratebox.util;
 
-import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.widget.EditText;
 
 /**
  * Utility class to display dialogue boxes
@@ -18,8 +20,8 @@ public class DialogUtil {
 	 * @param title		title to display
 	 * @param message	message to display
 	 */
-	public static void showDialog(Activity activity, int title, int message) {
-				AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+	public static void showDialog(Context context, int title, int message) {
+				AlertDialog.Builder builder = new AlertDialog.Builder(context);
 				builder.setTitle(title)
 						.setMessage(message)
 						.setPositiveButton(android.R.string.ok, null);
@@ -39,15 +41,45 @@ public class DialogUtil {
 	 * @param posListener		positive click listener
 	 * @param negListener		negative click listener
 	 */
-	public static void showDialog(Activity activity, int title, int message,
+	public static void showDialog(Context context, int title, int message,
 			int posButtonLabel, int negButtonLabel,
 			OnClickListener posListener, OnClickListener negListener) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+		AlertDialog.Builder builder = new AlertDialog.Builder(context);
 		builder.setTitle(title).setMessage(message)
 				.setPositiveButton(posButtonLabel, posListener)
 				.setNegativeButton(negButtonLabel, negListener);
 
 		AlertDialog dialog = builder.create();
 		dialog.show();
+	}
+	
+	/**
+	 * Displays an input dialog
+	 * 
+	 * @param title					title to display
+	 * @param body					message to show
+	 * @param input					input to use
+	 * @param iconId				icon to use, may be {@code null}
+	 * @param positiveListener		listener for positive button
+	 * @param negativeListener		listener for negative button
+	 */
+	public static void showInputDialog(Context context, CharSequence title,
+			CharSequence body, EditText input, int iconId,
+			DialogInterface.OnClickListener positiveListener,
+			DialogInterface.OnClickListener negativeListener) {
+				AlertDialog.Builder builder = new AlertDialog.Builder(context);
+				builder.setTitle(title);
+				builder.setIcon(iconId);
+				builder.setMessage(body);
+				builder.setView(input);
+
+				builder.setPositiveButton(context.getText(android.R.string.ok),
+						positiveListener);
+				builder.setNegativeButton(
+						context.getText(android.R.string.cancel),
+						negativeListener);
+
+				AlertDialog ad = builder.create();
+				ad.show();
 	}
 }
